c289f98c3f33ac108d075bf57781d8e7e339e197
Francescodl04 <72445602+Francescodl04@users.noreply.github.com>
2023-01-08 15:55:32 +0100

    Aggiunta progetto IP calculator
    Progetto ancora da completare

---
+++ IP_calculator/IP_calculator/IPv4.cs
@@ -0,0 +1,248 @@
+﻿/*
+ * Francesco Di Lena, classe 5F
+ * 7-01-2022
+ * Realizzazione di un software per la gestione degli indirizzi IP
+ * Sistemi e reti
+ * A.S. 2022-2023
+ * Classe IPv4: contiene tutti i metodi per svolgere le operazioni su IPv4
+*/
+
+using System.Net;
+
+namespace IP_calculator
+{
+    public class IPv4
+    {
+        IPAddress ipAddress;
+        IPAddress subnetMask;
+        int cidr = 0;
+
+        #region Metodi costruttori
+
+        //Primo costruttore
+
+        public IPv4(IPAddress ipAddress, IPAddress subnetMask)
+        {
+            this.ipAddress = ipAddress;
+            this.subnetMask = subnetMask;
+        }
+
+        //Secondo costruttore
+
+        public IPv4(IPAddress subnetMask)
+        {
+            this.subnetMask = subnetMask;
+        }
+
+        //Terzo costruttore
+
+        public IPv4(IPAddress ipAddress, int cidr)
+        {
+            this.ipAddress = ipAddress;
+            this.cidr = cidr;
+        }
+
+        public IPAddress GetIP()
+        {
+            return ipAddress;
+        }
+
+        #endregion
+
+        public void SetIPAddress(IPAddress ipAddress)
+        {
+            this.ipAddress = ipAddress;
+        }
+
+        #region Metodi relativi agli indirizzi IP di rete
+
+        public IPAddress GetNetworkAddress()
+        {
+            if (subnetMask == null)
+            {
+                subnetMask = GetSubnetMaskFromCIDR();
+            }
+            byte[] hostAddressBytes = ipAddress.GetAddressBytes();
+            byte[] subnetMaskBytes = subnetMask.GetAddressBytes();
+            byte[] networkAddressBytes = new byte[4];
+            string networkAddress = "";
+            for (int i = 0; i < 4; i++)
+            {
+                networkAddressBytes[i] = (byte)(hostAddressBytes[i] & subnetMaskBytes[i]);
+                if (i == 0)
+                {
+                    networkAddress = Convert.ToString(networkAddressBytes[i]);
+                }
+                else
+                {
+                    networkAddress = string.Concat(networkAddress, '.', networkAddressBytes[i]);
+                }
+            }
+            return IPAddress.Parse(networkAddress);
+        }
+        #endregion
+
+
+        #region Metodi relativi al CIDR, alle subnet e wildcard mask
+
+        public IPAddress GetSubnetMask()
+        {
+            return subnetMask;
+        }
+
+        public IPAddress GetWildCardMask()
+        {
+            return null;
+        }
+
+        public void SetSubnetMask(IPAddress subnetMask)
+        {
+            this.subnetMask = subnetMask;
+        }
+
+        public IPAddress GetSubnetMaskFromCIDR()
+        {
+            string binarySubnetMask = "";
+            string subnetMask = "";
+            for (int i = 0; i <= 32; i++)
+            {
+                if (i < cidr)
+                {
+                    binarySubnetMask = string.Concat(binarySubnetMask, '1');
+                }
+                else
+                {
+                    binarySubnetMask = string.Concat(binarySubnetMask, '0');
+                }
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                string temp = "";
+                for (int j = 0; j < 8; j++)
+                {
+                    temp = string.Concat(temp, binarySubnetMask[j + i * 8]);
+                }
+                if (i == 0)
+                {
+                    subnetMask = Convert.ToInt32(temp, 2).ToString();
+                }
+                else
+                {
+                    subnetMask = string.Concat(subnetMask, '.', Convert.ToInt32(temp, 2).ToString());
+                }
+            }
+            return IPAddress.Parse(subnetMask);
+        }
+
+        public int GetCIDR()
+        {
+            string binarySubnetMask = "";
+            int cidr = 0;
+            byte[] subnetMaskBytes = subnetMask.GetAddressBytes();
+            for (int i = 0; i < subnetMaskBytes.Length; i++)
+            {
+                binarySubnetMask = string.Concat(binarySubnetMask, Convert.ToString(subnetMaskBytes[i], 2));
+            }
+            for (int i = 0; i < binarySubnetMask.Length; i++)
+            {
+                if (binarySubnetMask[i] == '1')
+                {
+                    cidr++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return cidr;
+        }
+
+        #endregion
+
+
+        #region Metodi relativi al range di host di una rete
+
+        public IPAddress GetFirstHost()
+        {
+            byte[] networkBytes = GetNetworkAddress().GetAddressBytes();
+            networkBytes[3]++;
+            string firstHost = "";
+            for (int i = 0; i < 4; i++)
+            {
+                if (i == 0)
+                {
+                    firstHost = networkBytes[i].ToString();
+                }
+                else
+                {
+                    firstHost = string.Concat(firstHost, '.', networkBytes[i]);
+                }
+            }
+            return IPAddress.Parse(firstHost);
+        }
+
+        public IPAddress GetLastHost()
+        {
+            byte[] broadcastBytes = GetBroadcastAddress().GetAddressBytes();
+            broadcastBytes[3]--;
+            string lastHost = "";
+            for (int i = 0; i < 4; i++)
+            {
+                if (i == 0)
+                {
+                    lastHost = broadcastBytes[i].ToString();
+                }
+                else
+                {
+                    lastHost = string.Concat(lastHost, '.', broadcastBytes[i]);
+                }
+            }
+            return IPAddress.Parse(lastHost);
+        }
+
+        public IPAddress GetBroadcastAddress()
+        {
+            byte[] ipAddressBytes = ipAddress.GetAddressBytes();
+            byte[] subnetMaskBytes = subnetMask.GetAddressBytes();
+            byte[] broadcastBytes = new byte[4];
+            string broadcastAddress = "";
+            for (int i = 0; i < 4; i++)
+            {
+                broadcastBytes[i] = (byte)(ipAddressBytes[i] | subnetMaskBytes[i]);
+                if (i == 0)
+                {
+                    broadcastAddress = broadcastBytes[i].ToString();
+                }
+                else
+                {
+                    broadcastAddress = string.Concat(broadcastAddress, '.', broadcastBytes[i]);
+                }
+            }
+            return IPAddress.Parse(broadcastAddress);
+        }
+
+        public double GetTotalNumberHost()
+        {
+            if (cidr == 0)
+            {
+                cidr = GetCIDR();
+            }
+            int hostBits = 32 - cidr;
+            return Math.Pow(2, hostBits) - 2;
+        }
+
+        public IPAddress[] GetUsableHost()
+        {
+            IPAddress[] addresses = new IPAddress[] { GetFirstHost(), GetLastHost() };
+            return addresses;
+        }
+
+        #endregion 
+
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ IP_calculator/IP_calculator/Program.cs
@@ -0,0 +1,187 @@
+﻿/*
+ * Francesco Di Lena, classe 5F
+ * 7-01-2022
+ * Realizzazione di un software per la gestione degli indirizzi IP
+ * Sistemi e reti
+ * A.S. 2022-2023
+ * Classe Program: contiene tutti i metodi relativi alle visualizzazioni video
+*/
+
+using System;
+using System.IO;
+using System.Collections;
+using System.Dynamic;
+using System.Net;
+using System.Net.Sockets;
+
+namespace IP_calculator
+{
+    internal class Program
+    {
+        static readonly string[] menuText = new string[] { "ottieni l'indirizzo di rete a partire da un indirizzo host", "ottieni, a partire dall'indirizzo e la denominazione CIDR, la subnet mask", "ottiene la denominazione CIDR da una subnet mask", "ottieni il numero di host di una rete", "esci dal programma" };
+
+
+        #region Metodi di uso generale
+
+        static private void LogoScreen()
+        {
+            Console.Clear();
+            Console.WriteLine("    ________     ______      __           __      __            \r\n   /  _/ __ \\   / ____/___ _/ /______  __/ /___ _/ /_____  _____\r\n   / // /_/ /  / /   / __ `/ / ___/ / / / / __ `/ __/ __ \\/ ___/\r\n _/ // ____/  / /___/ /_/ / / /__/ /_/ / / /_/ / /_/ /_/ / /    \r\n/___/_/       \\____/\\__,_/_/\\___/\\__,_/_/\\__,_/\\__/\\____/_/     \r\n                                                                ");
+        }
+
+        static private IPAddress IPInsert()
+        {
+            Console.WriteLine("Inserisci l'indirizzo IP (nella forma x.y.z.h):");
+            IPAddress? ipAddress = null;
+            do
+            {
+                string ipAddressString = Console.ReadLine();
+                try
+                {
+                    ipAddress = IPAddress.Parse(ipAddressString);
+                    ipAddress = ipAddress.MapToIPv4();
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Non è stato inserito un indirizzo IP corretto, riprova...");
+                    continue;
+                }
+                break;
+            } while (true);
+            return ipAddress;
+        }
+
+        static private IPAddress SubnetMaskInsert()
+        {
+            Console.WriteLine("Inserisci la subnet mask (nella forma x.y.z.h):");
+            IPAddress? subnetMask = null;
+            do
+            {
+                string subnetMaskString = Console.ReadLine();
+                try
+                {
+                    subnetMask = IPAddress.Parse(subnetMaskString);
+                    subnetMask = subnetMask.MapToIPv4();
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Non è stata inserita una subnet mask corretta, riprova...");
+                    continue;
+                }
+                break;
+            } while (true);
+            return subnetMask;
+        }
+
+        static private int CIDRInsert()
+        {
+            Console.WriteLine("Inserisci la notazione CIDR dell'indirizzo IP (solamente il numero):");
+            int cidr = int.Parse(Console.ReadLine());
+            return cidr;
+        }
+
+        #endregion
+
+        static private void Main()
+        {
+            LogoScreen();
+            Console.WriteLine("Benvenuto nel software per calcolare tutti i dettagli sugli indirizzi IPv4! Scegli una delle opzioni del menu:\n");
+            for (int i = 0; i < menuText.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}) {menuText[i]}");
+            }
+            do
+            {
+                switch (Console.ReadKey(true).KeyChar)
+                {
+                    case '1':
+                        NetworkIpScreen();
+                        break;
+                    case '2':
+                        SubnetMaskScreen();
+                        break;
+                    case '3':
+                        CIDRScreen();
+                        break;
+                    case '4':
+                        HostScreen();
+                        break;
+                    case '5':
+                        ExitScreen();
+                        break;
+                    default:
+                        Console.WriteLine($"Devi inserire un numero compreso fra 1 e {menuText.Length}, riprova...");
+                        continue;
+                }
+                break;
+            } while (true);
+        }
+
+        #region Metodi per la visualizzazione dei menu
+
+        static private void NetworkIpScreen()
+        {
+            LogoScreen();
+            Console.WriteLine("Desideri inserire la notazione decimal dotted o CIDR per la subnet mask?\nInserisci \"d\" per la prima, \"c\" per la seconda:");
+            IPAddress networkAddress;
+            do
+            {
+                switch (Console.ReadKey(true).KeyChar)
+                {
+                    case 'd':
+                        Console.Write("Hai scelto di inserire la subnet mask. ");
+                        IPv4 ipv4 = new IPv4(IPInsert(), SubnetMaskInsert());
+                        networkAddress = ipv4.GetNetworkAddress();
+                        break;
+                    case 'c':
+                        Console.Write("Hai scelto di inserire la notazione CIDR. ");
+                        ipv4 = new IPv4(IPInsert(), CIDRInsert());
+                        networkAddress = ipv4.GetNetworkAddress();
+                        break;
+                    default:
+                        Console.WriteLine("Non hai inserito un carattere corretto, riprova...");
+                        continue;
+                }
+                break;
+            } while (true);
+            Console.WriteLine($"L'indirizzo di rete è \"{networkAddress}\"");
+            MenuReturn();
+        }
+
+        static private void SubnetMaskScreen()
+        {
+            LogoScreen();
+            IPv4 ipv4 = new IPv4(IPInsert(), CIDRInsert());
+            Console.WriteLine($"La subnet-mask richiesta è \"{ipv4.GetSubnetMaskFromCIDR()}\"");
+            MenuReturn();
+        }
+
+        static private void CIDRScreen()
+        {
+            LogoScreen();
+            IPv4 ipv4 = new IPv4(SubnetMaskInsert());
+            Console.WriteLine($"La denominazione CIDR della subnet mask inserita è \"\\{ipv4.GetCIDR()}\"");
+            MenuReturn();
+        }
+
+        static private void HostScreen()
+        {
+            LogoScreen();
+            MenuReturn();
+        }
+
+        static private void MenuReturn()
+        {
+            Console.WriteLine("Premi un tasto qualsiasi per tornare alla schermata iniziale...");
+            Console.ReadKey();
+            Main();
+        }
+
+        static private void ExitScreen()
+        {
+            Console.Clear();
+            Environment.Exit(0);
+        }
+        #endregion
+    }
+}
